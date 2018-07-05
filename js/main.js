6131fed88354a14b69a42aacb526cf663472aa38
DESKTOP-UCJUD62\ArtG <artem.garmatyk@gmail.cm>
2018-07-05 22:20:25 +0300

    add fixes

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-$('[data-fancybox]').fancybox({
-    loop: true,
-    protect: false
-});
-
-
-$(".containerForPolitic").fancybox({
-    selector: '[data-fancybox="#politic, #politic1, #politic2, #politic3"]',
-    loop: !0
-});
-
-$(".containerForSome").fancybox({
-    selector: '[data-fancybox="#some"]',
-    loop: !0
-});
-
-$(".containerForForm").fancybox({
-    selector: '[data-fancybox="#form1_1, #form1_2, #form1_3"]',
-    loop: !0
-});
-
-
-$.fancybox.defaults.animationEffect = "fade";
-
-if(screen.width <= 767) {
-    $('.catalog .flex-ct-2 .card').appendTo('.catalog .flex-ct-1');
-    $('.catalog .flex-ct-2').remove();
-    $('.catalog .flex-ct').addClass('fade')
-    $('.delivery .flex-ct').addClass('fade')
-    $('.partner .flex-ct').addClass('fade')
-}
-
-$('.fade').slick({
-    infinite: true,
-    speed: 500,
-    fade: true,
-    arrows: false,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-});
-
-ymaps.ready(init);
 function init(){
     // Создание карты.
     var myMap = new ymaps.Map("map", {
@@ -65,3 +22,8 @@ function init(){
 // можно добавить на карту.
     myMap.geoObjects.add(myPlacemark);
 }
+
+$(document).ready(function(){
+
+    ymaps.ready(init);
+})
---
+++ js/validate_script.js
@@ -0,0 +1,114 @@
+/*валидация формы*/
+function validate(form, options){
+    var setings = {
+        errorFunction:null,
+        submitFunction:null,
+        highlightFunction:null,
+        unhighlightFunction:null
+    }
+    $.extend(setings, options);
+
+    var $form = form;
+
+    if ($form.length && $form.attr('novalidate') === undefined) {
+        $form.on('submit', function(e) {
+            e.preventDefault();
+        });
+
+        $form.validate({
+            errorClass : 'errorText',
+            focusCleanup : true,
+            focusInvalid : false,
+            invalidHandler: function(event, validator) {
+                if(typeof(setings.errorFunction) === 'function'){
+                    setings.errorFunction(form);
+                }
+            },
+            errorPlacement: function(error, element) {
+                error.appendTo( element.closest('.form_input'));
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('error');
+                $(element).closest('.input').addClass('error').removeClass('valid');
+                if( typeof(setings.highlightFunction) === 'function' ) {
+                    setings.highlightFunction(form);
+                }
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('error');
+                if($(element).closest('.input').is('.error')){
+                    $(element).closest('.input').removeClass('error').addClass('valid');
+                }
+                if( typeof(setings.unhighlightFunction) === 'function' ) {
+                    setings.unhighlightFunction(form);
+                }
+            },
+            submitHandler: function(form) {
+                if( typeof(setings.submitFunction) === 'function' ) {
+                    setings.submitFunction(form);
+                } else {
+                    $form[0].submit();
+                }
+            }
+        });
+    }
+}
+
+/*Отправка формы с вызовом попапа*/
+function validationCall(form){
+
+    var thisForm = $(form);
+    var formSur = thisForm.serialize();
+
+    $.ajax({
+        url : thisForm.attr('action'),
+        data: formSur,
+        method:'POST',
+        success : function(data){
+            if ( data.trim() == true) {
+                thisForm.trigger("reset");
+                $.fancybox.close();
+                popNext("#call_success");
+            }
+            else {
+               thisForm.trigger('reset');
+            }
+
+        }
+    });
+}
+
+function popNext(popupId){
+
+    $.fancybox.open({
+        src  : popupId,
+        type : 'inline',
+        opts : {
+            afterClose: function(){
+                $('form').trigger("reset");
+            }
+        }
+    });
+
+}
+
+/*маска на инпуте*/
+function Maskedinput(){
+    if($('input[type=tel]')){
+        $('input[type=tel]').mask('+7 ( 999 ) 999 - 99 - 99');
+    }
+}
+
+
+
+$(document).ready(function(){
+
+    $('form').each( function(){
+
+        validate( $(this) , {submitFunction:validationCall}); 
+
+    })
+    
+    Maskedinput();
+
+});
